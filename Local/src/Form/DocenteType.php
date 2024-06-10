97a082d92b25d1176139320f9151c175ff0e491e
FazGit2211 <zuletafacundoadolfo2211@gmail.com>
2024-06-10 16:54:06 -0300

    Modificación
    Para que solo aparezcan para seleccionar las personas que no estén ya agregadas como un alumno y/o docente.

--- Local/src/Form/DocenteType.php
+++ Local/src/Form/DocenteType.php
@@ -3,11 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Docente;
+use App\Entity\Alumno;
 use App\Entity\Persona;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
 class DocenteType extends AbstractType
 {
@@ -17,6 +20,13 @@ class DocenteType extends AbstractType
             ->add('fecha_ingreso')
             ->add('persona',EntityType::class,[
                 'class' => Persona::class,
+                'query_builder' => function(EntityRepository $er) : QueryBuilder {
+                    return $er->createQueryBuilder('p')
+                    ->leftJoin('App\Entity\Alumno','a','WITH','p.id = a.persona')
+                    ->leftJoin('App\Entity\Docente','d','WITH','p.id = d.persona')
+                    ->where('a.persona IS NULL')
+                    ->andWhere('d.persona IS NULL');
+                },
                 'choice_label' => function($persona){
                     return 'Nombre : ' . $persona->getNombre() . ' , '. 'Apellido :' . $persona->getApellido() . ' , ' . 'Dni :' . $persona->getDnipasaporte();
                 },
